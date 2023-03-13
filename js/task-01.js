5d2d5e9f50f70ddb6ff90f7bec8ee9f05da7bb8b
maks-vynnychenko <vinnichenkomaks@gmail.com>
2023-03-13 23:34:06 +0200

    1/2

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
-console.log("Task 1");
+console.log('Task 1');
 
+const itemOFCategoriesEl = document.querySelectorAll('.item');
+console.log('Number of categories', itemOFCategoriesEl.length);
+
+itemOFCategoriesEl.forEach(item => {
+  console.log('Category:', item.firstElementChild.textContent);
+  console.log('Elements:', item.lastElementChild.children.length);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listsOfIngredientsEl = document.querySelector('#ingredients');
+
+const nameOfIngredients = ingredients.map(item => {
+  const liEl = document.createElement('li');
+  liEl.classList.add('item');
+  liEl.textContent = item;
+
+  return liEl;
+});
+
+listsOfIngredientsEl.append(...nameOfIngredients);
+
